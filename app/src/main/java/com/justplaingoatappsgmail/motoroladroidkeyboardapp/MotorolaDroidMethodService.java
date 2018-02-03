79d6b586b8fecd4e42e889e90ef25fe6b1829fa9
Michael Muinos <michaelmuinos@yahoo.com>
2018-02-03 15:20:58 -0800

    created keyboard layout

--- app/src/main/java/com/justplaingoatappsgmail/motoroladroidkeyboardapp/MotorolaDroidMethodService.java
+++ app/src/main/java/com/justplaingoatappsgmail/motoroladroidkeyboardapp/MotorolaDroidMethodService.java
@@ -1,6 +1,14 @@
 package com.justplaingoatappsgmail.motoroladroidkeyboardapp;
 
 import android.inputmethodservice.InputMethodService;
+import android.view.View;
 
 public class MotorolaDroidMethodService extends InputMethodService {
+
+    @Override
+    public View onCreateInputView() {
+        View view = getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        return view;
+    }
+
 }
