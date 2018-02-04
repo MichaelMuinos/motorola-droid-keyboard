3340cff1ad72a0189d71b3e0e8a92511c5165a36
Michael Muinos <michaelmuinos@yahoo.com>
2018-02-03 17:41:47 -0800

    add butterknife

--- app/src/main/java/com/justplaingoatappsgmail/motoroladroidkeyboardapp/MotorolaDroidMethodService.java
+++ app/src/main/java/com/justplaingoatappsgmail/motoroladroidkeyboardapp/MotorolaDroidMethodService.java
@@ -1,14 +1,34 @@
 package com.justplaingoatappsgmail.motoroladroidkeyboardapp;
 
 import android.inputmethodservice.InputMethodService;
+import android.util.Pair;
 import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+import java.util.Map;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MotorolaDroidMethodService extends InputMethodService {
+
+    private static final Map<Integer,Pair<Character,Character>> keyboardPairs = null;
+
+    static {
+
+    }
 
     @Override
     public View onCreateInputView() {
         View view = getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        ButterKnife.bind(this, view);
         return view;
+    }
+
+    @OnClick
+    public void onOneClick() {
+        InputConnection inputConnection = getCurrentInputConnection();
+        inputConnection.commitText("1", 1);
     }
 
 }
