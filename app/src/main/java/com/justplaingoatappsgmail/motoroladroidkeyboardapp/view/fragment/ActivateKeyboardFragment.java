9f0b31574238e61d67a62df6e11247bcdf6c27eb
Michael Muinos <michaelmuinos@yahoo.com>
2018-02-04 23:03:48 -0800

    add more

--- app/src/main/java/com/justplaingoatappsgmail/motoroladroidkeyboardapp/view/fragment/ActivateKeyboardFragment.java
+++ app/src/main/java/com/justplaingoatappsgmail/motoroladroidkeyboardapp/view/fragment/ActivateKeyboardFragment.java
@@ -1,65 +1,13 @@
 package com.justplaingoatappsgmail.motoroladroidkeyboardapp.view.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.justplaingoatappsgmail.motoroladroidkeyboardapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ActivateKeyboardFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ActivateKeyboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ActivateKeyboardFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public ActivateKeyboardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ActivateKeyboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ActivateKeyboardFragment newInstance(String param1, String param2) {
-        ActivateKeyboardFragment fragment = new ActivateKeyboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -68,42 +16,4 @@ public class ActivateKeyboardFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_activate_keyboard, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
--- app/src/main/java/com/justplaingoatappsgmail/motoroladroidkeyboardapp/view/fragment/TurnKeyboardOnFragment.java
+++ app/src/main/java/com/justplaingoatappsgmail/motoroladroidkeyboardapp/view/fragment/TurnKeyboardOnFragment.java
@@ -1,7 +1,10 @@
 package com.justplaingoatappsgmail.motoroladroidkeyboardapp.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +20,8 @@ import butterknife.OnClick;
 
 public class TurnKeyboardOnFragment extends Fragment {
 
+    private static final int INPUT_METHOD_SETTING_RESULT_CODE = 9000;
+
     @BindView(R.id.step_one_check_wrapper) RelativeLayout checkWrapper;
     @BindView(R.id.explanation_text_view) TextView explanation;
     @BindView(R.id.turn_on_button) BootstrapButton turnOnButton;
@@ -27,14 +32,27 @@ public class TurnKeyboardOnFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_turn_keyboard_on, container, false);
         ButterKnife.bind(this, view);
-
+        isTurnedOn();
 
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        // check if the keyboard is turned on or not
+    }
+
     @OnClick(R.id.turn_on_button)
     public void turnOnClick() {
+        Intent intent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+        startActivity(intent);
+    }
 
+    private boolean isTurnedOn() {
+        String enabledInputMethods = Settings.Secure.getString(getActivity().getContentResolver(), Settings.Secure.ENABLED_INPUT_METHODS);
+        Log.d("tag", enabledInputMethods);
+        return false;
     }
 
 }
